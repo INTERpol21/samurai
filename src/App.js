ff5eb2e78009dedfdc007d615d6f6179a6e7954a
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-09-04 19:46:34 +0600

    11.Lesson

--- src/App.js
+++ src/App.js
@@ -3,20 +3,50 @@ import './App.css';
 
 export const App = () => {
   return (
-    <div>
-      <header>
-        <img src="https://www.logoeps.net/wp-content/uploads/2018/04/tesla_motors-logo.png" alt='logo'/>
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://i.pinimg.com/originals/82/12/e3/8212e3207e68b986fa25423918bb91d7.png"
+          alt="logo"
+        />
       </header>
-      <nav>
+      <nav className="nav">
         <div>
-          Profile
+          <a href="#">Profile</a>
         </div>
         <div>
-          Message
+          <a href="#">Messages</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
         </div>
       </nav>
-      <div>
-        Main content
+      <div className="content">
+        <div>
+          <img
+            src="https://img.desktopwallpapers.ru/rocks/pics/wide/1920x1200/27640f370156a0e0ae3ee9608fc8480a.jpg"
+            alt="back"
+          />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My post
+        </div>
+        <div>
+          New post
+        </div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
       </div>
     </div>
   );
