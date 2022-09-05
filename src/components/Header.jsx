71b2b8075f198ef5f538acf819483ded9da5b2ac
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-09-05 14:43:53 +0600

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Header.jsx 	modified:   src/components/Navbar.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 
 
 const Header = () => {
-    return(
-    <header className="header">
-        <img
-            src="https://i.pinimg.com/originals/82/12/e3/8212e3207e68b986fa25423918bb91d7.png"
-            alt="logo"
-        />
+    return (
+        <header className="header">
+            <img
+                src="https://i.pinimg.com/originals/82/12/e3/8212e3207e68b986fa25423918bb91d7.png"
+                alt="logo"
+            />
         </header>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 
 
 const Navbar = () => {
-    return(
-    <nav className="nav">
-        <div>
-            <a href="#">Profile</a>
-        </div>
-        <div>
-            <a href="#">Messages</a>
-        </div>
-        <div>
-            <a href="#">News</a>
-        </div>
-        <div>
-            <a href="#">Music</a>
-        </div>
-        <div>
-            <a href="#">Settings</a>
-        </div>
+    return (
+        <nav className="nav">
+            <div>
+                <a href="#">Profile</a>
+            </div>
+            <div>
+                <a href="#">Messages</a>
+            </div>
+            <div>
+                <a href="#">News</a>
+            </div>
+            <div>
+                <a href="#">Music</a>
+            </div>
+            <div>
+                <a href="#">Settings</a>
+            </div>
         </nav>
     )
 }
