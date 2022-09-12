39628c4335bd9a95128da463b74ea070fd0c47b8
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-09-12 18:04:23 +0600

    35

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import state, {addPost, subscribe, updateNewPostText} from "./redux/state"
 
-import state  from "./redux/state"
-import {rerenderEntireTree} from "./render";
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
+ let rerenderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>
+    )
+}
 
 rerenderEntireTree(state);
+//Замыкание из state.js, вызываем только при rerenderEntireTree(state);
+ subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,44 +1,58 @@
-import {rerenderEntireTree} from "../render";
+//вызываем замыкание
+let rerenderEntireTree = () => {
+    console.log("State change")
+}
 
 let state = {
     profilePage: {
         posts: [{id: 1, message: "Hi, how are you?", likesCount: 12}, {
-            id: 2,
-            message: "It my first post",
-            likesCount: 22
+            id: 2, message: "It my first post", likesCount: 22
         }, {id: 3, message: "Hi, how are you?", likesCount: 32}, {
-            id: 4,
-            message: "It my first post",
-            likesCount: 42
+            id: 4, message: "It my first post", likesCount: 42
         }, {id: 5, message: "Hi, how are you?", likesCount: 52}, {id: 6, message: "It my first post", likesCount: 62},],
+        newPostText: ""
     }, dialogsPage: {
         dialogs: [{id: 1, name: "Антон"}, {id: 2, name: "Василий"}, {id: 3, name: "Екатерина"}, {
-            id: 4,
-            name: "Владимир"
-        }, {id: 5, name: "Нюша"}, {id: 6, name: "Слава"},],
-        messages: [{id: 1, message: "Lorem ipsum dolor sit amet"}, {
-            id: 2,
-            message: "Magnis dis parturient montes nascetur ridiculus mus mauris vitae."
+            id: 4, name: "Владимир"
+        }, {id: 5, name: "Нюша"}, {id: 6, name: "Слава"},], messages: [{id: 1, message: "Lorem ipsum dolor sit amet"}, {
+            id: 2, message: "Magnis dis parturient montes nascetur ridiculus mus mauris vitae."
         }, {id: 3, message: "Ac placerat vestibulum lectus mauris ultrices eros in cursus"}, {
-            id: 4,
-            message: "Lorem ipsum dolor sit amet"
+            id: 4, message: "Lorem ipsum dolor sit amet"
         }, {id: 5, message: "Magnis dis parturient montes nascetur ridiculus mus mauris vitae."}, {
-            id: 6,
-            message: "Lorem ipsum dolor sit amet"
+            id: 6, message: "Lorem ipsum dolor sit amet"
         },]
     }
 
 }
+//Проверка состояния и ввода данных в state в частности в newPostText
+// window.state = state;
 
-export let addPost = (postMessage) => {
+export const addPost = () => {
     let newPost = {
-        id: 7,
-        message: postMessage,
-        likesCount: 0
+        id: 7, //Вводим начальную строку
+        message: state.profilePage.newPostText, likesCount: 0
     };
 
+
     state.profilePage.posts.push(newPost);
+    //Делаем пустую строку после ввода
+    state.profilePage.newPostText = "";
+
     rerenderEntireTree(state);
+}
+
+export const updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
+
+    rerenderEntireTree(state);
+
+}
+
+export const subscribe = (observer) => {
+
+    //принимаем в родительском мире let rerenderEntireTree и присваиваем observer
+    rerenderEntireTree = observer //наблюдатель = observer патерн!
+
 }
 
 export default state;
